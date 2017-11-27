47d55e78be29d149572e300453b0c0dd9a47b94c
Co-Su <thisusernameisnottaken@gmail.com>
2017-11-27 15:21:21 -0800

    initial HTML and CSS established.  on to javascript

---
+++ javascript.js
@@ -0,0 +1,21 @@
+//--Let's get started!
+
+//--We need to establish characters.  4 of them.
+var characterOne = 
+//--characters have health and default attack power stats.
+
+//--Select a character, and they become "your character".  move remaining characters to "available to be attacked" 
+
+//--selecting a character changes their attack var to '5'.
+
+//--select an enemy and they move to the defender zone.
+
+//--add attack button functionality.
+
+//--attack button does the following: Reduce enemy HP by current player ATK, reduce player HP by enemy ATK, then raise player ATK var by its initial value.
+
+//--if enemy HP = 0, despawn enemy.
+
+//--if attack button is pressed and there are no enemies in defender zone, then zone displays text: "Nobody home"
+
+//--add reset button
