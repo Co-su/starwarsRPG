4399fd60b8558e2e974e32d67ae49f2e428e3899
Co-Su <thisusernameisnottaken@gmail.com>
2017-11-27 17:56:48 -0800

    major Javascript progress.  Need to program attack button and damage attributes

--- javascript.js
+++ javascript.js
@@ -1,10 +1,36 @@
 //--Let's get started!
 
 //--We need to establish characters.  4 of them.
-var characterOne = 
+var vader  = [25,120];
+var sidious  = [30,130];
+var kylo  = [35,100];
+var snoke  = [50,180];
 //--characters have health and default attack power stats.
+$("#vaderHealth").text(vader[1]);
+$("#sidHealth").text(sidious[1]);
+$("#kyloHealth").text(kylo[1]);
+$("#snokeHealth").text(snoke[1]);
 
+var arena = undefined;
 //--Select a character, and they become "your character".  move remaining characters to "available to be attacked" 
+jQuery(".characterCard").click(function() {
+	var selectChar = $(this);
+	//console.log(selectChar.parent())
+	// For selecting player character
+	if (selectChar.parent().attr("id") === "charSelect") {
+		$("#charSelect .characterCard").appendTo("#bench");
+		$("#"+selectChar.attr("id")).appendTo("#playerZone");
+	};
+	// For selecting enemy
+	if (selectChar.parent().attr("id") === "bench") {
+        if (arena === undefined) {
+		$("#"+selectChar.attr("id")).appendTo("#defenderZone");
+		arena = "assigned";
+	    };
+	};
+	
+});
+
 
 //--selecting a character changes their attack var to '5'.
 
