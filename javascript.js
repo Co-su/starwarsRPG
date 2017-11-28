915679efc89e40418dd17b61842a6e27f79fb108
Co-Su <thisusernameisnottaken@gmail.com>
2017-11-27 18:58:01 -0800

    more progress but ran out of time not complete

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 //--Let's get started!
 
 //--We need to establish characters.  4 of them.
-var vader  = [25,120];
-var sidious  = [30,130];
-var kylo  = [35,100];
-var snoke  = [50,180];
+var charOne = {charId:"vader", attack:25, health:120};
+var charTwo = {charId:"sidious", attack:30, health:130};
+var charThree = {charId:"kylo", attack:35, health:100};
+var charFour = {charId:"snoke", attack:50, health:180};
 //--characters have health and default attack power stats.
-$("#vaderHealth").text(vader[1]);
-$("#sidHealth").text(sidious[1]);
-$("#kyloHealth").text(kylo[1]);
-$("#snokeHealth").text(snoke[1]);
+$("#vaderHealth").text(charOne.health);
+$("#sidHealth").text(charTwo.health);
+$("#kyloHealth").text(charThree.health);
+$("#snokeHealth").text(charFour.health);
 
 var arena = undefined;
+var player = undefined;
+
 //--Select a character, and they become "your character".  move remaining characters to "available to be attacked" 
 jQuery(".characterCard").click(function() {
 	var selectChar = $(this);
@@ -20,23 +22,29 @@ jQuery(".characterCard").click(function() {
 	if (selectChar.parent().attr("id") === "charSelect") {
 		$("#charSelect .characterCard").appendTo("#bench");
 		$("#"+selectChar.attr("id")).appendTo("#playerZone");
+		$("#"+selectChar.attr("id")).attr("id","player")
+		player = eval(selectChar.attr("id"));
 	};
 	// For selecting enemy
 	if (selectChar.parent().attr("id") === "bench") {
         if (arena === undefined) {
 		$("#"+selectChar.attr("id")).appendTo("#defenderZone");
-		arena = "assigned";
+		$("#"+selectChar.attr("id")).attr("id","enemy")
+		arena = eval(selectChar.attr("id"));
 	    };
 	};
-	
 });
-
 
 //--selecting a character changes their attack var to '5'.
 
-//--select an enemy and they move to the defender zone.
-
 //--add attack button functionality.
+
+$("#buttonOfDoom").click(function() {
+	player.health = player.health - arena.attack;
+	(player.health).appendTo("player.health")
+	arena.health = arena.health - player.attack;
+	player.attack += 8; 
+}) ;
 
 //--attack button does the following: Reduce enemy HP by current player ATK, reduce player HP by enemy ATK, then raise player ATK var by its initial value.
 
